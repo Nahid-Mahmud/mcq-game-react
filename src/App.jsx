fde6efa696ca352b265cbcd4ec1a3dc991cb85cd
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-12-15 00:43:19 +0600

    react query setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-function App() {
+import React from 'react';
+
+const App = () => {
+
   return (
-    <>
-      <div className="text-center ">Main page</div>
-    </>
+    <div>
+       <div className="text-center ">Main page</div>
+    </div>
   );
-}
+};
 
 export default App;
+
+
+// https://raw.githubusercontent.com/Nahid-Mahmud/mcq-game-react/main/public/quiz.json
